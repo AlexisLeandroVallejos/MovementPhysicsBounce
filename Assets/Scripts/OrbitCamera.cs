9a9ed0d3dd3c19e6d2106e6f476cd18994b9b444
Alexis Leandro Vallejos <49035235+AlexisLeandroVallejos@users.noreply.github.com>
2022-11-28 10:45:14 -0300

    agregado manteniendo posicion relativa

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -3,4 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-public class OrbitCamera : MonoBehaviour {}
+public class OrbitCamera : MonoBehaviour 
+{
+    //objetivo a seguir
+    [SerializeField]
+    Transform focus = default;
+
+    //distancia al objetivo a seguir
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+    //actualizar tardio para seguir con la camera luego de que Update() pase
+    void LateUpdate()
+    {
+        //lugar/cosa a enfocar
+        Vector3 focusPoint = focus.position;
+
+        //donde apuntar la camara
+        Vector3 lookDirection = transform.forward;
+
+        //mover camara: posicion de la esfera - posicion de la camara por su distancia
+        transform.localPosition = focusPoint - lookDirection * distance;
+    }
+}
