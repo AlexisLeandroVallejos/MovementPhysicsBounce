b318ef802b9197a8f7aec25bf678db25190c34fb
Alexis Leandro Vallejos <49035235+AlexisLeandroVallejos@users.noreply.github.com>
2022-11-29 11:47:08 -0300

    agregada direccion normalizada

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -107,11 +107,17 @@ public class MovingSphere : MonoBehaviour
 
         //si hay espacio de entrada del jugador...
         if(playerInputSpace){
+
+            //normalizar forward y right (XZ) para que la camara no afecte la velocidad deseada de la esfera al rotar su orbita vertical
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
             
             //la velocidad deseada sera modificada segun este espacio
-            desiredVelocity = playerInputSpace.TransformDirection(
-                playerInput.x, 0f, playerInput.y
-            ) * maxSpeed;
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
         }
         //sino usarla directamente de los controles de entrada
         else{
